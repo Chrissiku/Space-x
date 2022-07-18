76befd06a18ff0582e137f329fb611f820390e9a
Grayce Muthui <graycemuthui20@gmail.com>
2022-07-18 18:08:35 +0300

    Create Missions basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Mission from './components/Missions';
 
 function App() {
   return (
     <>
       <h1>Space-x group project</h1>
+      <Mission />
     </>
   );
 }
