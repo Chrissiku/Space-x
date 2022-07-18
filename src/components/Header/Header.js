a9c815e76f43867b62618d745546d504202d0c20
chrissiku <chrissiku5@gmail.com>
2022-07-18 13:51:12 +0200

    Split Header component into multiple jsx components'

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,26 +2,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
 
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Logo from "./logo";
+import NavMenu from "./Navbar";
 
 function Header() {
   return (
     <Navbar className="navbar">
-      <Navbar.Brand className="brand">
-        <img
-          src="https://user-images.githubusercontent.com/101924220/179498899-67ed3078-c059-42c0-8175-57ed7ec93c58.png"
-          width="70"
-          height="70"
-          alt="space-x logo"
-        />
-        <span>Space Travelers&apos; Hub</span>
-      </Navbar.Brand>
-      <Nav className="nav-link">
-        <Nav.Link className="link active">Rockets</Nav.Link>
-        <Nav.Link className="link">Missions</Nav.Link>
-        <Nav.Link className="link">Profile</Nav.Link>
-      </Nav>
+      <Logo />
+      <NavMenu />
     </Navbar>
   );
 }
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+import Nav from "react-bootstrap/Nav";
+
+const NavMenu = () => (
+  <Nav className="nav-link">
+    <Nav.Link className="link">Rockets</Nav.Link>
+    <Nav.Link className="link">Missions</Nav.Link>
+    <Nav.Link className="link">My Profile</Nav.Link>
+  </Nav>
+);
+
+export default NavMenu;
