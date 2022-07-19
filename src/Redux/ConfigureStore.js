b6306323be7f1c8d4de38c1bd03746be08e84c5c
chrissiku <chrissiku5@gmail.com>
2022-07-19 19:20:18 +0200

    Fix log errors

--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -1,8 +1,9 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable object-curly-newline */
 /* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
 
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import RocketsReducer from "./rockets";
@@ -12,6 +13,11 @@ const reducer = combineReducers({
   rockets: RocketsReducer,
   missions: MissionsReducer,
 });
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger)),
+);
 
 export default store;
