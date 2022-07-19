4b9264991f650587056988d8f9903e1dac3eb029
Grayce Muthui <graycemuthui20@gmail.com>
2022-07-19 15:19:35 +0300

    Create tests for the mission componentr

---
+++ src/tests/Mission.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import store from "../Redux/ConfigureStore";
+import Missions from "../components/Missions/Missions";
+
+it("Missions render correctly", () => {
+  const Tree = TestRenderer.create(
+    <Provider store={store}>
+      <Missions />
+    </Provider>
+  );
+  expect(Tree).toMatchSnapshot();
+});
