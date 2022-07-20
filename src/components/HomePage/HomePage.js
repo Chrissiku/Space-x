dd9f01f3e7decf54f470b0ff0ecacd4f6df9734c
chrissiku <chrissiku5@gmail.com>
2022-07-20 22:30:31 +0200

    Add a video Background to the Home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,7 +6,16 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <>
-      <Link to="main/*">Go TO main page</Link>
+      <div className="homepage">
+        <video className="v-background" autoPlay muted loop>
+          <source
+            src="https://user-images.githubusercontent.com/101924220/180069597-84a570d5-b256-416e-9110-085082cf074d.mp4"
+            type="video/mp4"
+          />
+          Browser is not supporting this web site assets
+        </video>
+        <Link to="main/*">Go TO main page</Link>
+      </div>
     </>
   );
 }
