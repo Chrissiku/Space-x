870818189acb9da38a72c7f5a8418432de19d935
chrissiku <chrissiku5@gmail.com>
2022-07-20 19:48:50 +0200

    Refactor the APP routing

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <>
+      <Link to="main/*">Go TO main page</Link>
+    </>
+  );
+}
+
+export default HomePage;
--- src/components/MainPage/MainPage.js
+++
@@ -1,7 +0,0 @@
-import Container from "react-bootstrap/Container";
-
-function MainPage() {
-  return <Container></Container>;
-}
-
-export default MainPage;
